335e94b191e5809bf8a6b21be36fce47e5d4292e
MaslovskayaMarina <90597493+MaslovskayaMarina@users.noreply.github.com>
2021-10-31 22:34:02 +0200

    Delete Pr_8 directory

--- Pr_8/ex2/SortingStudentsByGPA.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-
-public class SortingStudentsByGPA implements Comparator {
-    @Override
-    public int compare(Object A, Object B) {
-        return ((Student) A).getGPA() - ((Student) B).getGPA();
-    }
-}
